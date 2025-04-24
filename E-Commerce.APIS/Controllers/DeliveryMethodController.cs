642885ec23f3f7dcb07945bf10e98023f95e30d8
احمد 31107050 <UG_31107050@ics.tanta.edu.eg>
2025-04-24 15:31:19 +0200

    done

---
+++ E-Commerce.APIS/Controllers/DeliveryMethodController.cs
@@ -0,0 +1,24 @@
+﻿using E_Commerce.Core.Order_Aggregrate;
+using E_Commerce.Core.Repositories.Contract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.APIS.Controllers
+{
+   
+    public class DeliveryMethodController :BaseApiController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeliveryMethodController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<DeliveryMethod>>> GetAllDeliveryMethod()
+        {
+            var delivery= await _unitOfWork.Repository<DeliveryMethod>().GetAllAsync();
+            return Ok(delivery);
+        }
+    }
+}
